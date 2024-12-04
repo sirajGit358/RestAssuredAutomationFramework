462bc8e5142a9953d47e00e80b4a823cf4379d9c
sirajGit358 <sumkhan358@gmail.com>
2024-12-05 01:44:35 +0530

    my first commit

---
+++ src/test/java/api/endPoints/Routes.java
@@ -0,0 +1,18 @@
+package api.endPoints;
+
+public class Routes {
+
+//	public static String BASE_URL="https://petstore.swagger.io/v2";
+//	public static String POST_URL=BASE_URL + "/user";
+//	public static String GET_URL=BASE_URL + "/user/{username}";
+//	public static String PUT_URL=BASE_URL + "/user/{username}";
+//	public static String DELETE_URL=BASE_URL + "/user/{username}";
+	
+	public static String base_url = "https://petstore.swagger.io/v2";
+	
+	public static String  post_url = base_url + "/user";
+	public static String  get_url = base_url + "/user/{username}";
+	public static String  put_url = base_url + "/user/{username}";
+	public static String  del_url = base_url + "/user/{username}";
+
+}
